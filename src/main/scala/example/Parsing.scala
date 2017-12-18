e3019744846b095b8942ffef88ee53e5f6c5486b
AesaKamar <aesa.kamar@gmail.com>
2017-12-18 09:48:15 -0500

    Made more composable parser combinators

--- src/main/scala/example/Parsing.scala
+++ src/main/scala/example/Parsing.scala
@@ -9,38 +9,37 @@ import pprint._
 object Parsing {
   val validChar = P(CharIn("-123456789X/"))
 
-  val pointParser = P(CharIn("-123456789").!).map(p => Point(p.toInt))
-  val strikeParser = P("X")
-  val spareParser = P("/")
+  val pointParser = P(CharIn("-123456789").!).map {
+    case "-" => Point(0)
+    case p   => Point(p.toInt)
+  }
+  val strikeParser = P("X").map(_ => Strike)
+  val spareParser = P("/").map(_ => Spare)
+
+  val specialBowlParser : Parser[S_Bowl] =  strikeParser | spareParser
+  val bowlParser : Parser[Bowl] = pointParser | specialBowlParser
+
+
+  val pairParser : Parser[(Point, Bowl)]= pointParser ~ (pointParser | spareParser)
+
+  // This is a hack because scala does not implement Sum types for ADTs nicely, so type inference breaks down.
+  // Possibly fixable by using Shapeless Generic[] to convert the tuples to case class instances
+  val normalFrameParser: Parser[NormalFrame] = (pairParser | strikeParser).map{
+    case (p1 : Point, Spare) => FN_Spare(p1, Spare)
+    case (p1: Point, p2: Point) => FN_Point(p1, p2)
+    case Strike => FN_Strike(Strike)
+  }
+
+  val bonusFrameParser : Parser[BonusFrame] =
+    ((pointParser ~ pointParser) | (bowlParser ~ specialBowlParser ~ bowlParser)).map{
+    case (p1: Point, p2: Point) => FB_Normal(p1, p2)
+    case (b1: Bowl, sb2: S_Bowl, b3: Bowl) => FB_Special(b1, sb2, b3)
+  }
+
+  val frameParser : Parser[Frame] = normalFrameParser | bonusFrameParser
 
   def parseFrames(input: String): Option[List[Frame]] = {
-
-    val inputSansWhitespace = input.filterNot(_.isWhitespace)
-    val res = P(validChar.! ~ validChar.! ~ validChar.?.!).rep.parse(inputSansWhitespace)
-
-    res match {
-      case Success(frames, _) =>
-        frames.toList.traverse(parseFrame)
-      case Failure(_, _, _) => None
-    }
+    ???
   }
 
-  def parseFrame(attempt: (String, String, String)) : Option[Frame] = {
-    attempt match{
-        //This is the case of a Normal Frame
-      case (b1, b2, "") => {
-        (pointParser.parse(b1), pointParser.parse(b2)) match {
-            case (Success(p1, _), Success(p2, _)) => Some(FN_Point(p1, p2))
-          }
-
-
-      }
-        //This is the case of a Bonus Frame
-      case (b1, b2, b3) => {
-
-        ???
-      }
-    }
-
-  }
 }
--- src/test/scala/example/ParsingTest.scala
+++ src/test/scala/example/ParsingTest.scala
@@ -4,12 +4,52 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FreeSpec, Matchers}
 import org.scalatest.OptionValues._
 import fastparse._
+import fastparse.core.Parsed.{Failure, Success}
 
-class ParsingTest extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks{
 
-  "Parsing into normal frames" - {
+class ParsingTest
+    extends FreeSpec
+    with Matchers
+    with GeneratorDrivenPropertyChecks {
+
+  "Testing individual parsers" - {
+    import Parsing._
     "11" in {
-      Parsing.parseFrames("11").value shouldBe List(FN_Point(Point(1), Point(1)))
+      pairParser.parse("11") shouldBe Success((Point(1), Point(1)), 2)
+    }
+    "1-" in {
+      pairParser.parse("1-") shouldBe Success((Point(1), Point(0)), 2)
+    }
+    "1/" in {
+      pairParser.parse("1/") shouldBe Success((Point(1), Spare), 2)
+    }
+    "Normal Frame" - {
+      "11" in {
+        normalFrameParser.parse("11") shouldBe Success(FN_Point(Point(1), Point(1)), 2)
+      }
+      "1/" in {
+        normalFrameParser.parse("1/") shouldBe Success(FN_Spare(Point(1), Spare), 2)
+      }
+      "X" in {
+        normalFrameParser.parse("X") shouldBe Success(FN_Strike(Strike), 1)
+      }
+    }
+    "Bonus Frame" - {
+      "11" in {
+        bonusFrameParser.parse("11") shouldBe Success(FB_Normal(Point(1), Point(1)), 2)
+      }
+      "1/X" in {
+        bonusFrameParser.parse("1/X") shouldBe Success(FB_Special(Point(1), Spare, Strike), 3)
+      }
+      "XXX" in {
+        bonusFrameParser.parse("XXX") shouldBe Success(FB_Special(Strike, Strike, Strike), 3)
+      }
+    }
+
+  }
+
+  "Parsing into normal frames" ignore {
+    "Pointed" - {
     }
   }
 
